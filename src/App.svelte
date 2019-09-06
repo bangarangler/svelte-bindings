4d09bcdf31ffe1db599fb2a73d052efe70d0483a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-06 09:23:53 -0400

    custom bindings... don't overuse this mainly good for forms

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
+  import CustomInput from './CustomInput.svelte';
+  import Toggle from './Toggle.svelte';
+
   let val = "";
+  let selectedOption = 1;
+
+  $: console.log(val)
+  $: console.log(selectedOption)
+
+  function setValue(e) {
+    val = e.target.value
+  }
 </script>
 
 <input type="text" value={val}>
+<input type="text" value={val} on:input={setValue}>
+<input type="text" bind:value={val}>
+<CustomInput bind:val={val} />
+<Toggle bind:chosenOption={selectedOption} />
