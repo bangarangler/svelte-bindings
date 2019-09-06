aa38c066c588a6d6d725b961d16480f31cff4034
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-06 09:42:39 -0400

    many ways to deal with bindings, select, radio, checked, number, etc...

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,19 @@
 
   let val = "";
   let selectedOption = 1;
+  let price = 0;
+  let agreed;
+  let favColor = 'green';
+  let favColorCheck = ['green']
+  let singleFavColor = 'blue';
 
   $: console.log(val)
   $: console.log(selectedOption)
+  $: console.log(price)
+  $: console.log(agreed)
+  $: console.log(favColor)
+  $: console.log(favColorCheck)
+  $: console.log(singleFavColor)
 
   function setValue(e) {
     val = e.target.value
@@ -18,3 +28,48 @@
 <input type="text" bind:value={val}>
 <CustomInput bind:val={val} />
 <Toggle bind:chosenOption={selectedOption} />
+
+<input type="number" value={price} on:input={e => console.log('1' +
+       e.target.value)} />
+
+<input type="number" bind:value={price} />
+
+<label>
+  <input type="checkbox" bind:checked={agreed} />
+  Agree to Terms?
+</label>
+
+<h1>Favorite Color?</h1>
+<label>
+  <input type="radio" name="color" value="red" bind:group={favColor}>
+  red
+</label>
+<label>
+  <input type="radio" name="color" value="green" bind:group={favColor}>
+  green
+</label>
+<label>
+  <input type="radio" name="color" value="blue" bind:group={favColor}>
+  blue
+</label>
+
+<label>
+  <input type="checkbox" name="color" value="red" bind:group={favColorCheck}>
+  red
+</label>
+<label>
+  <input type="checkbox" name="color" value="green"
+  bind:group={favColorCheck}>
+  green
+</label>
+<label>
+  <input type="checkbox" name="color" value="blue"
+  bind:group={favColorCheck}>
+  blue
+</label>
+
+<select bind:value={singleFavColor}>
+  <option value="green">Green</option>
+  <option value="red">Red</option>
+  <option value="blue">Blue</option>
+</select>
